862bb021125cfd3ab1435469579a07ce129f5022
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-03-10 12:18:06 +0000

    hello world

---
+++ template.js
@@ -0,0 +1,9 @@
+// Typing Animation Script Using jQuery CDN
+const typed = new Typed(".typing", {
+  strings: ["", "a Web Designer", "a Youtuber", "a Freelancer", "a Graphic Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+// 
